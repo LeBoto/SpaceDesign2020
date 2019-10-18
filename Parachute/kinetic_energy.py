c75c6e3a874b34e781982a6af9e3268ffea835e5
Leboto <astnd5@gmail.com>
2019-10-18 13:14:44 -0400

    addition of KE calculation

---
+++ Parachute/kinetic_energy.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def kinetic_energy(m, v):
+    return 0.5*m*v**2
--- Parachute/velocity.py
+++ Parachute/velocity.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def velocity(weight, diam, c_d, rho, m, g):
+def velocity(diam, c_d, rho, m, g):
     v = sqrt((8*m*g) / (np.pi*rho*c_d*diam**2))
     return v
