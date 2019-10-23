3d2aba8d5fb03b43cab70fb8f516a2f0652ba843
AJ <astnd5@gmail.com>
2019-10-22 20:14:43 -0400

    Added drift calculation for payload

--- Parachute/descent_numerical.py
+++ Parachute/descent_numerical.py
@@ -27,7 +27,7 @@ equ_main = descent_equ(mass, sa, cd_para, cd_drift, v_wind)
 # sim setup Payload
 mass = 0.217567
 diam = 3.0
-cd_para = 1.50
+cd_para = 1.00
 cd_drift = 0.45
 v_wind = 10.0  # ft/s
 sa = area(diam)
@@ -43,6 +43,7 @@ y = res_d[-1, :]
 res_m, time12 = sim(y, dt, equ_main, breakm)
 res_p, time13 = sim(y, dt, equ_pay, breakm)
 time12 += time01[-1]
+time13 += time01[-1]
 res_full = np.append(res_d, res_m, axis=0)
 time_r = np.append(time01, time12, axis=0)
 time_p = np.append(time01, time13, axis=0)
@@ -54,21 +55,25 @@ plt.xlabel("x (ft)")
 plt.ylabel("y (ft)")
 plt.figure(2)
 plt.plot(time_r, res_full[:, 2])
+plt.plot(time13, res_p[:, 2])
 plt.title("Drift Velocity")
 plt.xlabel("time (s)")
 plt.ylabel("velocity (ft/s")
 plt.figure(3)
 plt.plot(time_r, res_full[:, 3])
+plt.plot(time13, res_p[:, 3])
 plt.title("Descent Velocity")
 plt.xlabel("time (s)")
 plt.ylabel("velocity (ft/s")
 
-# g = 32.17405  # ft/s^2
-# rho = 0.0023769  # slug/ft^3
-# vel = velocity(diam, cd_para, rho, mass, g)
+g = 32.17405  # ft/s^2
+rho = 0.0023769  # slug/ft^3
+print("____________________________________")
+print("Analytical Terminal Velocity Main: {} ft/s".format(velocity(8.0, 2.59, rho, 1.61621 - 0.217567, g)))
+print("AnalyticalTerminal Velocity Payload: {} ft/s".format(velocity(3.0, 1.0, rho, 0.217567, g)))
 print("____________________________________")
 print("Descent time rocket: {0:.2f} secs".format(time_r[-1]))
-print("Descent time rocket: {0:.2f} secs".format(time_p[-1]))
+print("Descent time payload: {0:.2f} secs".format(time_p[-1]))
 print("Terminal velocity drogue: {0:.2f} ft/s".format(res_d[-1, 3]))
 print("Terminal velocity main: {0:.2f} ft/s".format(res_m[-1, 3]))
 print("Terminal velocity payload: {0:.2f} ft/s".format(res_p[-1, 3]))
--- Parachute/velocity.py
+++ Parachute/velocity.py
@@ -2,5 +2,6 @@ import numpy as np
 
 
 def velocity(diam, c_d, rho, m, g):
-    v = np.sqrt((8*m*g) / (np.pi*rho*c_d*diam**2))
+    area = np.pi*(diam/2)**2
+    v = np.sqrt((2*m*g) / (rho*c_d*area))
     return v
