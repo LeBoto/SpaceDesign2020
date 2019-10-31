00809e9be9613306ec46b5bb856db1cdf2a84260
Alex Stubbles <astnd5@gmail.com>
2019-10-31 08:20:34 -0400

    added displays for numerical simulation

--- Parachute/alex_sim/descent_numerical.py
+++ Parachute/alex_sim/descent_numerical.py
@@ -17,7 +17,9 @@ equ_drogue = descent_equ(m_rocket, sa, cd_para)
 # sim setup Main
 m_rocket = lb2slug(30.5)
 cd_para = 2.59
+cd_para = 2.92
 sa = 89.0
+sa = 129.0
 equ_main = descent_equ(m_rocket, sa, cd_para)
 
 # sim setup Payload free fall
@@ -109,3 +111,18 @@ plt.axvline(x=90.0, label="Descent time requirement")
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (ft/s)")
 plt.legend()
+
+print("Kinetic Energy of Vehicle on Touchdown: \t\t\t{0:.2f} ft lbs".format(kinetic_energy(rocket_sim[-1, 1], m_rocket)))
+print("Kinetic Energy of Canister with Drone on Touchdown: {0:.2f} ft lbs".format(kinetic_energy(payload_e_sim[-1, 1], m_can + m_drone)))
+print("Kinetic Energy of Canister on Touchdown: \t\t\t{0:.2f} ft lbs".format(kinetic_energy(payload_sim[-1, 1], m_can)))
+print("Kinetic Energy of Nosecone on Touchdown: \t\t\t{0:.2f} ft lbs".format(kinetic_energy(payload_sim[-1, 1], m_nose)))
+print()
+print("Max Velocity of Vehicle under Drogue: \t\t\t{0:.2f} ft/s".format(abs(res_d[-1, 1])))
+print("Max Velocity of Vehicle under Main Parachute:  \t{0:.2f} ft/s".format(abs(rocket_sim[-1, 1])))
+print("Max Velocity of Payload: \t\t\t\t\t\t{0:.2f} ft/s".format(abs(res_f[-1, 1])))
+print("Max Velocity of Payload + Drone under parachute:{0:.2f} ft/s".format(abs(payload_e_sim[-1, 1])))
+print("Max Velocity of Payload under parachute: \t\t{0:.2f} ft/s".format(abs(payload_sim[-1, 1])))
+print()
+print("Descent Time of Vehicle:  \t\t{0:.2f} s".format(rocket_time[-1]))
+print("Descent Time of Payload: \t\t{0:.2f} s".format(payload_time[-1]))
+print("Descent Time of Payload + Drone:{0:.2f} s".format(payload_e_time[-1]))
--- Parachute/drift.py
+++
@@ -1,3 +0,0 @@
-def drift(v, v_wind, c_d, diam, rho):
-    area = np.pi * diam**2.0 / 4.0
-    return 0.5*rho*(v_wind - v)**2*area + c_d*0.5*rho*(v_wind - v)**2*area
