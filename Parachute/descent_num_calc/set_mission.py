c858e788f3753daf3bc77dc0d1a9e1f15da3d903
AJ <astnd5@gmail.com>
2019-11-21 13:24:59 -0500

    fixed breaking condition

--- Parachute/descent_num_calc/set_mission.py
+++ Parachute/descent_num_calc/set_mission.py
@@ -5,6 +5,7 @@ import numpy as np
 class mission(object):
     def __init__(self, n_phases, initial_state, time_step, break_alt, masses, chutes, max_time, max_ke, alt_state=0):
         assert n_phases == len(break_alt) == len(masses)
+        self.__as = alt_state
         self.n = n_phases
         self.dt = time_step
         self.time_lim = max_time
@@ -12,15 +13,17 @@ class mission(object):
         self.phase = break_alt
         self.equ = []
         self.state = initial_state
-        self.__breaks = []
         for i in range(n_phases):
             self.equ.append(self.make_equ(masses[i], chutes[i].S, chutes[i].cd))
-            self.__breaks.append(lambda state: state[alt_state] > self.phase[i])
+
+    def bcon(self, y, bc):
+        s = self.__as
+        return y[s] > bc
 
     def make_equ(self, mass, area, c_d):
         r_y, v_y = sy.symbols("r_y, v_y")
-        g = 32.17405  # ft/s^2
-        rho = 0.0023769  # slug/ft^3
+        g = 32.17405
+        rho = 0.0023769
         d_r_y = v_y
 
         d_v_drag = (c_d * 0.5 * rho * area * v_y ** 2) / mass
@@ -32,23 +35,22 @@ class mission(object):
         return l_equ
 
     def run_mission(self):
-        bc = self.__breaks
         y = self.state
         time = np.array([0])
         state = y
         state = state.reshape((1, len(y)))
         for i in range(self.n):
-            y, t = self.sim(y, self.dt[i], self.equ[i], bc[i])
+            y, t = self.sim(y, self.dt[i], self.equ[i], i)
             time = np.append(time, t + time[-1])
             state = np.append(state, y, axis=0)
             y = y[-1, :]
         return state, time
 
-    def sim(self, y_i, dt, func, bc):
+    def sim(self, y_i, dt, func, phase):
         y = y_i
         results = []
         it = 0
-        while bc(y):
+        while self.bcon(y, self.phase[phase]):
             it += 1
             y = self.rk4(y, func, dt)
             results.append(y)
