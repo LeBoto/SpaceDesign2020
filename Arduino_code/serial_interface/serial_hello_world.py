ce7c9627646b090de1d7d30a1944fceb75cd35d1
Administrator <astnd@gmail.com>
2020-01-02 16:34:16 -0500

    add arduino reader

---
+++ Arduino_code/serial_interface/serial_hello_world.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 13:25:46 2017
+
+@author: glassbox
+"""
+
+"""Module importation"""
+import serial
+"""Opening of the serial port"""
+try:
+    arduino = serial.Serial("COM7", timeout=1)
+except:
+    print('Please check the port')
+
+"""Receiving data and storing it in a list"""
+data = str(arduino.readline())
+parsed_data = data.split(',')
