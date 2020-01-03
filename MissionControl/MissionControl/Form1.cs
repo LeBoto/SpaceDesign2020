9c9d8259cc67d9d6d2c510b5a1604e9133409a93
Alex Stubbles <astnd5@gmail.com>
2020-01-03 05:22:04 -0500

    Adding GUI code idk if it works anywhere else

---
+++ MissionControl/MissionControl/Form1.cs
@@ -0,0 +1,124 @@
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Globalization;
+using GMap.NET;
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace MissionControl
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort mySerialPort = new SerialPort("COM13");
+        string[] parsed;
+        bool fix;
+        bool save_flag = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            mySerialPort.Open();
+            map.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            map.MinZoom = 5;
+            map.MaxZoom = 20;
+            map.Zoom = 15;
+
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            try
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    tb_raw.Text = indata;
+                });
+            }
+            catch(System.InvalidOperationException)
+            {
+
+            }
+            parsed = parse_string(indata);
+            update_text(parsed);
+        }
+
+        private string[] parse_string(string input_data)
+        {
+            string[] alldat = input_data.Split(',');
+            return alldat;
+        }
+
+        private void update_text(string[] dat_delim)
+        {
+            int equ;
+            int mer;
+            double lat = 0.0;
+            double lon = 0.0;
+
+            if (dat_delim.Length <= 1) return; // Checking for data
+            if (dat_delim[4] == "") fix = false; // if the latitude string is empty there is no fix
+            else fix = true;
+
+            if (dat_delim[5] == "N") equ = 1;
+            else equ = -1;
+
+            if (dat_delim[7] == "E") mer = 1;
+            else mer = -1;
+            if (fix)
+            {
+                lat = Convert.ToDouble(get_geo(dat_delim[4])) * equ;
+                lon = Convert.ToDouble(get_geo(dat_delim[6])) * mer;
+            }
+            // This will update all GUI elements whenever new data is received
+            try
+            {
+                this.Invoke((MethodInvoker)delegate
+            {
+                tb_lat.Text = lat.ToString();
+                tb_long.Text = lon.ToString();
+                btn_fix.Checked = fix;
+                if (fix) map.Position = new PointLatLng(lat, lon);
+            });
+            }
+            catch (System.InvalidOperationException)
+            {
+                // This happens when closing the GUI
+            }
+        }
+
+        private string get_geo(string coord)
+        {
+            float geo = float.Parse(coord, CultureInfo.InvariantCulture.NumberFormat);
+            int dd = (int)geo / 100;
+            float ss = geo - (float)dd * 100;
+            geo = dd + ss / 60;
+            return geo.ToString("0.00000");
+        }
+
+        private void btn_data_save_Click(object sender, EventArgs e)
+        {
+            if (!save_flag) btn_data_save.Text = "Stop Saving";
+            else btn_data_save.Text = "Start Saving";
+
+            save_flag = !save_flag;
+        }
+    }
+}
